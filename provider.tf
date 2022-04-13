c0542d3618d6c149cebd495af26852202b5ddc5b
minhvuong0499 <minhvuong0499@gmail.com>
2022-04-13 03:30:11 -0700

    first commit

--- provider.tf
+++ provider.tf
@@ -12,8 +12,8 @@ terraform {
 # Setup AWS provider
 provider "aws" {
   region     = var.aws_region
-  access_key = "AKIATUR7ZDHQTPXNOJPS"
-  secret_key = "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"
+  access_key = ""
+  secret_key = "kT39rPQA7McltxSxPiazLV6pVmamYWvZhqNYzyg"
 }
 
 resource "aws_key_pair" "aws-key" {
